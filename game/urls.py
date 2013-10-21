96e92e0236511fa578fcdd36701e17146669ad96
hm <kobe6672823@qq.com>
2013-10-21 10:22:29 +0800

    change the method of trying the db into 'exists()'

--- game/urls.py
+++ game/urls.py
@@ -16,5 +16,6 @@ urlpatterns = patterns('',
     url(r'^chapter/(?P<id>\d+)/child/?$', views.getChildChapter, name='getChildChapter'),
     url(r'^chapter/(?P<id>\d+)/children/?$', views.getChildrenChapters, name='getChildrenChapters'),
     url(r'^chapter/(?P<id>\d+)/offspring/?$', views.getOffspring, name='getOffspring'),
+    #url(r'^storyList/(?P<type>\d+)/(?P<start>\d+)/(?P<count>\d+)/(?P<timeStamp>\d+)/(?P<startStoryId>\d+)/?$', views.getStoryList, name='getStoryList'),
 )
 
--- mytest/testGetStory.py
+++ mytest/testGetStory.py
@@ -4,6 +4,6 @@
 import requests
 import json
 
-r = requests.post('http://127.0.0.1:8000/story/1')
+r = requests.post('http://127.0.0.1:8000/story/3')
 print r.text
 
